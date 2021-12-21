cec899026e2f92f235f092e85fe2b31312ddf0d6
Jakub Holeček <jakub.holecek31@gmail.com>
2021-12-21 14:14:22 +0100

    added second solution of first day

--- src/views/Aoc1.vue
+++ src/views/Aoc1.vue
@@ -1,8 +1,15 @@
 <template>
-  <h3>Day 1</h3>
+  <h2>Day 1</h2>
+  <h4>Part One</h4>
+  <!-- ţest code [199, 200, 208, 210, 200, 207, 240, 269, 260, 263] -->
   <p>
     {{ getNumOfLargerMeasur(inputImport) }} measurements are larger than the
     previous measurement.
+  </p>
+  <h4>Part two</h4>
+  <p>
+    {{ getNumOfLargerTreeMeasur(inputImport) }}
+    tree measurements are larger than the previous tree measurement.
   </p>
 </template>
 
@@ -19,6 +26,7 @@ export default {
 
   setup() {
     const inputImport = ref(input);
+
     return {
       inputImport,
     };
@@ -27,14 +35,24 @@ export default {
   methods: {
     getNumOfLargerMeasur(input) {
       let largerMeasurement = 0;
-
       for (let i = 1; i <= input.length; i++) {
         if (input[i] > input[i - 1]) {
           largerMeasurement += 1;
         }
       }
-
       return largerMeasurement;
+    },
+
+    getNumOfLargerTreeMeasur(input) {
+      let largerTreeMeasurement = 0;
+      for (let i = 0; i <= input.length; i++) {
+        const firstTree = input[i] + input[i + 1] + input[i + 2];
+        const secondTree = input[i + 1] + input[i + 2] + input[i + 3];
+        if (firstTree < secondTree) {
+          largerTreeMeasurement += 1;
+        }
+      }
+      return largerTreeMeasurement;
     },
   },
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <h1>Advent of code 2021</h1>
+  <div>Process: 1/50 completed</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
+
+<style>
+h2 {
+  text-decoration: underline;
+  padding-bottom: 20px;
+}
+</style>
