926ec54f87fa8705ec45ac19ff15b6b92e439d91
Jakub Holeček <jakub.holecek31@gmail.com>
2021-12-21 13:46:08 +0100

    Created Advent of Code project + first solution

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/aoc1">1.</router-link>
   </div>
   <router-view />
 </template>
---
+++ src/views/Aoc1.vue
@@ -0,0 +1,41 @@
+<template>
+  <h3>Day 1</h3>
+  <p>
+    {{ getNumOfLargerMeasur(inputImport) }} measurements are larger than the
+    previous measurement.
+  </p>
+</template>
+
+<script>
+import input from "../data/input1.1";
+import { ref } from "vue";
+
+export default {
+  name: "",
+
+  components: {},
+
+  props: {},
+
+  setup() {
+    const inputImport = ref(input);
+    return {
+      inputImport,
+    };
+  },
+
+  methods: {
+    getNumOfLargerMeasur(input) {
+      let largerMeasurement = 0;
+
+      for (let i = 1; i <= input.length; i++) {
+        if (input[i] > input[i - 1]) {
+          largerMeasurement += 1;
+        }
+      }
+
+      return largerMeasurement;
+    },
+  },
+};
+</script>
